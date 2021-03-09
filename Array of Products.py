c16428c83d26c03410d213d20492f891ba88e0f3
Hopertz <lugano.ngulwa@gmail.com>
2021-03-09 20:09:51 +0300

    initial commit

--- Array of Products.py
+++ Array of Products.py
@@ -1,20 +1,57 @@
-# Array of Products
+"""
+   Array of Products
+
+   Write a function that takes in a non-empty array of integers and returns an array of the
+   same length, where each element in the output array is equal to the product of every
+   other number in the input array.
+
+   In other words,the value at output[i] is equal to the product of every number in the
+   input array other than input[i].
+
+   Note that you're expected to solve this problem without using division
+
+   Sample Input
+     array = [5, 1, 4, 2]
+
+   Sample Output
+     [8, 40, 10, 20]
+
+     // 8 is equal to 1 x 4 x 2
+     // 40 is equal to 5 x 4 x 2
+     // 10 is equal to 5 x 1 x 2
+     // 20 is equal to 5 x 1 x 4
+
+"""
+
 
 # O(n^2) time | O(n) space where n is the length of array.
 def arrayOfProducts(array):
-    product = []
+    products = [1 for _ in range(len(array))]
     counter = 0
-    while counter < len(array):
-        running_prod = 1
-        for indx in range(len(array)):
-            if indx != counter:
-                running_prod *= array[indx]
+    for i in range(len(array)):
+        runningProduct = 1
+        for j in range(len(array)):
+            if i != j:
+                runningProduct *= array[j]
 
-        product.append(running_prod)
+        products[i] = runningProduct
         counter += 1
 
-    return product
+    return products
 
 
-# Input array = [5, 1, 4, 2]
-# Output [8, 40, 10, 20]
+# O(n) time | O(n) space where n is the length of array.
+def arrayOfProducts(array):
+    products = [1 for _ in range(len(array))]
+
+    leftRunningProduct = 1
+    for i in range(len(array)):
+        products[i] = leftRunningProduct
+        leftRunningProduct *= array[i]
+
+    rightRunningProduct = 1
+    for i in reversed(range(len(array))):
+        products[i] *= rightRunningProduct
+        rightRunningProduct *= array[i]
+
+    return products
--- Kadane's Algorithm.py
+++ Kadane's Algorithm.py
@@ -1,4 +1,19 @@
-# Kadane's Algorithm
+"""
+  Kadane's Algorithm
+
+  Write a function that takes in a non-empty array of integers and returns the
+  maximum sum that can be obtained by summing up all of the integers in a non-empty
+  subarray  of the input array.A subarray must only contain adjacent numbers (numbers
+  next to each other in the input array).
+
+  Sample Input
+    array = [3, 5,-9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+
+  Sample Output
+    19  // [1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1]
+
+
+"""
 
 # O(n) time | O(1) space where n is the length of the input array.
 def kadanesAlgorithm(array):
