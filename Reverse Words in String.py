3f13a93527295ce836bf1e7c37dac84ee38a9763
Hopertz <lugano.ngulwa@gmail.com>
2021-03-19 00:13:42 +0300

    initial commit

--- Reverse Words in String.py
+++ Reverse Words in String.py
@@ -20,26 +20,76 @@
     string = "AlgoExperts is the best!"
 
   Sample Output
-     "best the is AlgoExpert"
+     "best! the is AlgoExperts"
 """
 
 
-def reverseString(string):
-    res = ''
-    new = ''
+# time O(n*w) | space O(1) n is the length of string.
+# w is the length of new word formed.
+def reverseWordsInString(string):
+    result = ''
+    newWord = ''
     for indx in range(len(string) - 1, -1, -1):
         if string[indx] == ' ' or indx == 0:
             space = ' ' if not indx == 0 else ''
             begin = string[indx] if indx == 0 else ''
-            word = ''
-            for j in range(len(new) - 1, -1, -1):
-                word += new[j]
-            res += begin + word + space
-            new = ''
+            arrangeWord = ''
+            for j in range(len(newWord) - 1, -1, -1):
+                arrangeWord += newWord[j]
+            result += begin + arrangeWord + space
+            newWord = ''
             continue
-        new += string[indx]
-    return res
+        newWord += string[indx]
+    return result
 
 
+# time O(n) | space O(n) where n is the length of string.
+def reverseWordsInString(string):
+    words = []
+    startofWord = 0
+    for idx in range(len(string)):
+        character = string[idx]
+
+        if character == " ":
+            words.append(string[startofWord:idx])
+            startofWord = idx
+        elif string[startofWord] == " ":
+            words.append(" ")
+            startofWord = idx
+
+    words.append(string[startofWord:])
+
+    reverseList(words)
+    return "".join(words)
 
 
+def reverseList(list):
+    start, end = 0, len(list) - 1
+    while start < end:
+        list[start], list[end] = list[end], list[start]
+        start += 1
+        end -= 1
+
+
+# time O(n) | space O(n) where n is the length of string.
+def reverseWordsInString(string):
+    characters = [char for char in string]
+    reverseListRange(characters, 0, len(characters) - 1)
+
+    startofWord = 0
+    while startofWord < len(characters):
+        endofWord = startofWord
+        while endofWord < len(characters) and characters[endofWord] != " ":
+            endofWord += 1
+
+            reverseListRange(characters, startofWord, endofWord - 1)
+            startofWord = endofWord + 1
+
+    return "".join(characters)
+
+
+def reverseListRange(list, start, end):
+    while start < end:
+        list[start], list[end] = list[end], list[start]
+        start += 1
+        end -= 1
