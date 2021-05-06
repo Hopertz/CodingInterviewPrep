eff74cdcd99b621753854a5e1efe644252399929
Hopertz <lugano.ngulwa@gmail.com>
2021-05-06 20:52:13 +0300

    initial commit

---
+++ Subarray Sort.py
@@ -0,0 +1,15 @@
+
+"""
+   Subarray Sort
+   
+Write a function that takes in an array of at least two integers and that returns an array of the starting 
+and ending indices of the smallest subarray in the input array that needs to be sorted in place in order for 
+the entire input array to be sorted (in ascending order).
+If the input array is already sorted, the function should return [-1, -1] .
+
+Sample Input
+    array = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
+
+Sample Output
+    [3, 9]
+"""
