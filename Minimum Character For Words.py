6ec520a062c1bdcc2b1facd310d540dcf9bc4721
Hopertz <lugano.ngulwa@gmail.com>
2021-05-21 22:35:22 +0300

    initial commit

---
+++ Minimum Character For Words.py
@@ -0,0 +1,18 @@
+"""
+   Minimum Character for Words
+
+Write a function that takes in an array of words and returns the smallest array of characters needed to form all of the
+words. The characters don't need to be in any particular order.
+
+For example, the characters ["y", "r", "0", "U"] are needed to form the words
+["your", "you", "or", "yo"] .
+Note: the input words won't contain any spaces; however, they might contain punctuation and/or special characters.
+Sample Input
+  words = ["this", "that", "did", "deed", "them!", "a"]
+
+Sample Output
+   ["t", "t", "h", "i", "s", "a", "d", "d", "e", "e", "m", "!"]
+// The characters could be ordered differently.
+
+
+"""
