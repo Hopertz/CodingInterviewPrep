00287e6b527d09bec9f33d7f8deba2612bd3fadf
Lugano Abel <lugano.ngulwa@gmail.com>
2021-07-09 15:09:31 +0300

    Updated Min Max Stack Construction.py

--- Min Max Stack Construction.py
+++ Min Max Stack Construction.py
@@ -30,3 +30,35 @@
    peek(): 5
 
 """
+# Feel free to add new properties and methods to the class.
+class MinMaxStack:
+	 def __init__(self):
+	    self.minMaxStack = []
+		 self.stack = []
+		
+	 # O(1) time | O(1) space
+    def peek(self):
+        return self.stack[len(self.stack) - 1]
+	
+    # O(1) time | O(1) space
+    def pop(self):
+		  self.minMaxStack.pop()
+        return self.stack.pop()
+
+	 # O(1) time | O(1) space
+    def push(self, number):
+        newMinMax = {'min': number, 'max': number}
+        if len(self.minMaxStack):
+			  lastMinMax = self.minMaxStack[len(self.minMaxStack)-1]
+			  newMinMax['min'] = min(lastMinMax['min'], number)
+			  newMinMax['max'] = max(lastMinMax['max'], number)
+		  self.minMaxStack.append(newMinMax)
+		  self.stack.append(number)
+		
+    # O(1) time | O(1) space
+    def getMin(self):
+        return self.minMaxStack[len(self.minMaxStack)-1]['min']
+	
+    # O(1) time | O(1) space
+    def getMax(self):
+        return self.minMaxStack[len(self.minMaxStack)-1]['max']
