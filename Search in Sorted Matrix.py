876320a02b60f278f9717c387f1a755879ad09f3
Lugano Abel <lugano.ngulwa@gmail.com>
2021-07-09 10:24:04 +0300

    updated Search in Sorted Matrix.py

--- Search in Sorted Matrix.py
+++ Search in Sorted Matrix.py
@@ -22,3 +22,30 @@
     [3, 3]
 
 """
+
+
+# O(n + m) time | O(1) space
+def searchInSortedMatrix(matrix, target):
+    row = 0
+	col = len(matrix[0]) - 1
+	while row < len(matrix) and col >= 0:
+		if matrix[row][col] > target:
+			col -= 1
+		elif matrix[row][col] < target:
+			row += 1
+		else:
+			return [row,col]
+	return [-1, -1]
+			
+		
+		
+	
+ # O(n * m) time | O(1) space
+ def searchInSortedMatrix(matrix, target):
+     for row in range(len(matrix)):		
+	     for col in range(len(matrix[row])):
+		     if matrix[row][col] == target:
+			    return [row,col]
+	 else:
+		 return [-1,-1]
+			
