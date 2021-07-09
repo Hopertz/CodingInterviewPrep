fa2266acd38097f635cd011f726533a31299e266
Lugano Abel <lugano.ngulwa@gmail.com>
2021-07-09 10:27:33 +0300

    Updated Search in Sorted Matrix.py

--- Search in Sorted Matrix.py
+++ Search in Sorted Matrix.py
@@ -27,15 +27,15 @@
 # O(n + m) time | O(1) space
 def searchInSortedMatrix(matrix, target):
     row = 0
-	col = len(matrix[0]) - 1
-	while row < len(matrix) and col >= 0:
-		if matrix[row][col] > target:
-			col -= 1
-		elif matrix[row][col] < target:
-			row += 1
-		else:
-			return [row,col]
-	return [-1, -1]
+    col = len(matrix[0]) - 1
+    while row < len(matrix) and col >= 0:
+	  if matrix[row][col] > target:
+	     col -= 1
+	  elif matrix[row][col] < target:
+	       row += 1
+	  else:
+	      return [row,col]
+    return [-1, -1]
 			
 		
 		
@@ -43,9 +43,9 @@ def searchInSortedMatrix(matrix, target):
  # O(n * m) time | O(1) space
  def searchInSortedMatrix(matrix, target):
      for row in range(len(matrix)):		
-	     for col in range(len(matrix[row])):
-		     if matrix[row][col] == target:
-			    return [row,col]
-	 else:
-		 return [-1,-1]
+	 for col in range(len(matrix[row])):
+	     if matrix[row][col] == target:
+	        return [row,col]
+     else:
+         return [-1,-1]
 			
