f70d4d961423199d5eda3a514e4123f44d1bc339
Lugano Abel <lugano.ngulwa@gmail.com>
2021-07-10 13:32:04 +0300

    Updated Valid Starting City.py

--- Valid Starting City.py
+++ Valid Starting City.py
@@ -33,4 +33,48 @@
 
 """
 
+# O(n*2) time | O(1) space
+def validStartingCity(distances, fuel, mpg):
+    numberOfCities = len(distances)
+	
+	for startCityIdx in range(numberOfCities):
+		milesRemaining = 0
+		
+		for currentCityIdx in range(startCityIdx, startCityIdx + numberOfCities):
+			if milesRemaining < 0:
+				continue
+			currentCityIdx = currentCityIdx % numberOfCities
+			
+			fuelFromCurrentCity = fuel[currentCityIdx]
+			distanceToNextCity = distances[currentCityIdx]
+			milesRemaining += fuelFromCurrentCity * mpg - distanceToNextCity
+
+        if milesRemaining >= 0:
+		    return startCityIdx
+	
+	return -1
+
+
+# O(n) time | O(1) space
+def validStartingCity(distances, fuel, mpg):
+    numberOfCities = len(distances)
+    milesRemaining = 0
+	
+	indexOfStartingCityCandidate = 0
+	milesRemainingAtStartingcityCandidate = 0
+	
+	for cityIdx in range(1, numberOfCities):
+		distanceFromPreviousCity = distances[cityIdx - 1]
+		fuelFromPreviousCity = fuel[cityIdx - 1]
+		milesRemaining += fuelFromPreviousCity * mpg - distanceFromPreviousCity 
+		
+		if milesRemaining < milesRemainingAtStartingcityCandidate:
+			milesRemainingAtStartingcityCandidate = milesRemaining
+			indexOfStartingCityCandidate = cityIdx
+			
+	return indexOfStartingCityCandidate
+			
+
+
+
 
