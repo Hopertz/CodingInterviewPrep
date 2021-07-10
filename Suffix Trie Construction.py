39d11e4659229b6588006e69ae6dcb9261f8c396
Lugano Abel <lugano.ngulwa@gmail.com>
2021-07-10 16:19:03 +0300

    updated Suffix Trie Construction.py

--- Suffix Trie Construction.py
+++ Suffix Trie Construction.py
@@ -32,3 +32,37 @@
        true
    
 """
+
+# Do not edit the class below except for the
+# populateSuffixTrieFrom and contains methods.
+# Feel free to add new properties and methods
+# to the class.
+class SuffixTrie:
+    def __init__(self, string):
+        self.root = {}
+        self.endSymbol = "*"
+        self.populateSuffixTrieFrom(string)
+    
+	 # O(n^2) time | O(n^2) space
+    def populateSuffixTrieFrom(self, string):
+        for i in range(len(string)):
+	         self.insertSubstringStartingAt(i,string)
+	
+    def insertSubstringStartingAt(self, i, string):
+	     node = self.root
+	     for j in range(i, len(string)):
+	         letter = string[j]
+	         if letter not in node:
+	            node[letter] = {}
+	         node = node[letter]
+		      node[self.endSymbol] = True
+		
+    # O(m) time | O(1) space
+    def contains(self, string):
+        node = self.root
+        for letter in string:
+            if letter not in node:
+               return False
+            node = node[letter]
+        return self.endSymbol in node
+        
