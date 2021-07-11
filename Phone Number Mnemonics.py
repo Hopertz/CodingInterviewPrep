47c318f18ebb204193ac0fc8b45eb679714d8f4b
Lugano Abel <lugano.ngulwa@gmail.com>
2021-07-11 14:38:39 +0300

    Updated phone Number Mnemonics.py

--- Phone Number Mnemonics.py
+++ Phone Number Mnemonics.py
@@ -54,3 +54,36 @@ Note that you should rely on the keypad illustrated above for digit-letter assoc
 
 
 """
+
+DIGIT_LETTERS = {
+	'0' :['0'],
+	'1' :['1'],
+	'2' :['a','b','c'],
+	'3' :['d','e','f'], 
+	'4' :['g','h','i'], 
+	'5' :['j','k','l'],
+	'6' :['m','n','o'],
+	'7' :['p','q','r','s'],
+	'8' :['t','u','v'],
+	'9' :['w','x','y','z']
+}
+
+def phoneNumberMnemonics(phoneNumber):
+     currentMnemonic = ['0'] * len(phoneNumber)
+     mnemonicsFound = []
+     
+     phoneNumberMnemonicsHelper(0, phoneNumber, currentMnemonic, mnemonicsFound)
+     return mnemonicsFound
+
+def  phoneNumberMnemonicsHelper(idx, phoneNumber, currentMnemonic, mnemonicsFound):
+     if idx == len(phoneNumber):
+         mnemonic = ''.join(currentMnemonic)
+         mnemonicsFound.append(mnemonic)
+     else:
+          digit = phoneNumber[idx]
+          letters = DIGIT_LETTERS[digit]
+          for letter in letters:
+               currentMnemonic[idx] = letter
+               phoneNumberMnemonicsHelper(idx + 1, phoneNumber, currentMnemonic, mnemonicsFound)
+               
+     
