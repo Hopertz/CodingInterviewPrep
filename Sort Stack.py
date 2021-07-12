b979118bc1d69db3d9464234db3df7f26cbd6894
Lugano Abel <lugano.ngulwa@gmail.com>
2021-07-12 09:21:20 +0300

    Updated Sort Stack.py

--- Sort Stack.py
+++ Sort Stack.py
@@ -23,3 +23,26 @@ Sample Output
   [-5, -2, 1, 2, 3, 4]
 
 """
+
+# O(n^2) time | O(n) space - where n is the length of the stack
+def sortStack(stack):
+    if len(stack) == 0:
+       return stack
+    top = stack.pop()
+    sortStack(stack)
+    insertInSortedOrder(stack, top)
+    return stack
+
+def insertInSortedOrder(stack, value):
+    if len(stack) == 0 or stack[len(stack) - 1] <= value:
+       stack.append(value)
+       return
+    top = stack.pop()
+    insertInSortedOrder(stack, value)
+    stack.append(top)
+
+
+
+    
+            
+            
