5db6f98d321865f9c710134643bde04cfec7a991
Lugano Abel <lugano.ngulwa@gmail.com>
2021-07-13 11:38:28 +0300

    Updated Minimum Character For Words.py

--- Minimum Character For Words.py
+++ Minimum Character For Words.py
@@ -16,3 +16,43 @@ Sample Output
 
 
 """
+
+# O(n * l) time | O(c) space - where n is the number of words,l is the length of the longest word, 
+# and c is the number of unique characters across all words.
+# O(c) is lowerbound and O(n*l) is upperbound for the space complexity.
+
+def minimumCharactersForWords(words):
+   maximumCharacterFrequencies = {}
+   for word in words:
+      characterFrequencies = countCharacterFrequencies(word)
+      updateMaximumFrequencies(characterFrequencies, maximumCharacterFrequencies)
+   return makeArrayFromCharacterFrequencies(maximumCharacterFrequencies)
+
+def countCharacterFrequencies(string):
+   characterFrequencies = {}
+   for character in string:
+      if character not in characterFrequencies:
+         characterFrequencies[character] = 0
+      characterFrequencies[character] += 1
+   return characterFrequencies
+
+def updateMaximumFrequencies(frequencies, maximumFrequencies):
+   for character in frequencies:
+      frequency = frequencies[character]
+      if character in maximumFrequencies:
+         maximumFrequencies[character] = max(frequency, maximumFrequencies[character])
+      else:
+         maximumFrequencies[character] = frequency
+         
+def makeArrayFromCharacterFrequencies(characterFrequencies):
+   characters = []
+   for character in characterFrequencies:
+      frequency = characterFrequencies[character]
+      
+      for _ in range(frequency):
+         characters.append(character)
+    return characters
+         
+      
+
+
